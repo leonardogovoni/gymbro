2de81bc3617ecd49b0d97e615ba95441542a4181
Biagio.Gavioli <biagio.gavioli@gmail.com>
2024-12-02 08:28:59 -0800

    GYM CARDS!!!!

---
+++ database/seeders/GymCardsSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class GymCardsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('workout_plans')->insert([
+            [
+                'user_id' => 1,
+                'title' => 'Gambe Gambose',
+                'description' => 'Placeholder',
+                'start' => '07/12/2024',
+                'end' => '07/12/2025',
+                'enabled' => true,
+            ]
+        ]);
+    }
+}
