ffc1026e559e303abb3deb0d0339733ecc0b3af2
leonardogovoni <leo@leonardogovoni.com>
2024-12-04 22:01:17 +0000

    Aggiunto seeder con una scheda con esercizi

--- database/seeders/ExercisesSeeder.php
+++ database/seeders/ExercisesSeeder.php
@@ -14,36 +14,42 @@ class ExercisesSeeder extends Seeder
     {
         DB::table('exercises')->insert([
             [
+                'id' => 1,
                 'name' => 'Leg Extension',
                 'image' => 'leg_extension.png',
                 'description' => 'Placeholder',
                 'muscle' => 'Quadricipiti',
             ],
             [
+                'id' => 2,
                 'name' => 'Pressa 45°',
                 'image' => 'pressa_45.png',
                 'description' => 'Placeholder',
                 'muscle' => 'Quadricipiti',
             ],
             [
+                'id' => 3,
                 'name' => 'Hack Squat',
                 'image' => 'hack_squat.png',
                 'description' => 'Placeholder',
                 'muscle' => 'Quadricipiti',
             ],
             [
+                'id' => 4,
                 'name' => 'Leg Curl',
                 'image' => 'leg_curl.png',
                 'description' => 'Placeholder',
                 'muscle' => 'ischio-crurali',
             ],
             [
+                'id' => 5,
                 'name' => 'Leg Curl Seduto',
                 'image' => 'leg_curl_seduto.png',
                 'description' => 'Placeholder',
                 'muscle' => 'ischio-crurali',
             ],
             [
+                'id' => 6,
                 'name' => 'Calf Machine In Piedi',
                 'image' => 'calf_machine.png',
                 'description' => 'Placeholder',
--- database/seeders/GymCardsSeeder.php
+++ database/seeders/GymCardsSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class GymCardsSeeder extends Seeder
@@ -14,12 +14,54 @@ class GymCardsSeeder extends Seeder
     {
         DB::table('workout_plans')->insert([
             [
+                'id' => 1,
                 'user_id' => 1,
-                'title' => 'Gambe Gambose',
+                'title' => 'Scheda Principiante',
                 'description' => 'Placeholder',
                 'start' => '07/12/2024',
                 'end' => '07/12/2025',
                 'enabled' => true,
+            ],
+        ]);
+
+        DB::table('workout_plan_exercises')->insert([
+            // Giorno 1
+            [
+                'workout_plan_id' => 1,
+                'exercise_id' => 2,
+                'day' => 1,
+                'sequence' => 1,
+                'series' => 4,
+                'repetitions' => 12,
+                'rest' => 1.30,
+            ],
+            [
+                'workout_plan_id' => 1,
+                'exercise_id' => 1,
+                'day' => 1,
+                'sequence' => 2,
+                'series' => 4,
+                'repetitions' => 15,
+                'rest' => 1.30,
+            ],
+            [
+                'workout_plan_id' => 1,
+                'exercise_id' => 4,
+                'day' => 1,
+                'sequence' => 3,
+                'series' => 4,
+                'repetitions' => 12,
+                'rest' => 1.30,
+            ],
+            // Giorno 2
+            [
+                'workout_plan_id' => 1,
+                'exercise_id' => 6,
+                'day' => 2,
+                'sequence' => 1,
+                'series' => 4,
+                'repetitions' => 12,
+                'rest' => 1.30,
             ],
         ]);
     }
