0231ea9361cbf4b846ddccacc8ec14071dd1a539
Alchyon <seba2001borghi@gmail.com>
2024-12-09 18:37:58 +0100

    Pagina visualizzazione schede e icone

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,7 +22,7 @@
                     </x-nav-link>
                 </div>
 				<div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('workout_plans.list')" :active="request()->routeIs('workout_plan.list')">
+                    <x-nav-link :href="route('workout_plans.list')" :active="request()->routeIs('workout_plans.list')">
                         {{ __('Schede') }}
                     </x-nav-link>
                 </div>
@@ -87,7 +87,7 @@
 			</x-responsive-nav-link>
 		</div>
 		<div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('workout_plans.list')" :active="request()->routeIs('workout_plan.list')">
+            <x-responsive-nav-link :href="route('workout_plans.list')" :active="request()->routeIs('workout_plans.list')">
                 {{ __('Schede') }}
             </x-responsive-nav-link>
         </div>
