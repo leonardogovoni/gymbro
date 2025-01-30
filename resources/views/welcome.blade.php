bcdedc00979cc8f457c416a028c6ab79c74e662e
leonardogovoni <leo@leonardogovoni.com>
2025-01-30 20:12:02 +0000

    Finita home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,20 +14,20 @@
 		@vite(['resources/css/app.css', 'resources/js/app.js'])
 	</head>
 	<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-		<div class="pt-16 mx-auto lg:py-32 md:px-8 xl:px-36 w-full min-h-screen lg:flex items-center justify-between hero">
-			<div class="w-[520px] text-white">
-				<div class="flex">
+		<div class="pt-16 lg:pt-0 lg:px-8 xl:px-16 lg:flex w-full min-h-screen items-center justify-between hero">
+			<div class="w-full lg:w-[520px] text-white">
+				<div class="flex lg:justify-start justify-center">
 					<x-application-logo class="h-32 fill-white" />
 					<h2 class="ms-2 max-w-lg my-auto font-sans text-6xl font-bold tracking-tight ">
 						Gymbro
 					</h2>
 				</div>
 				
-				<p class="md:text-lg">
+				<p class="px-4 lg:px-0 text-center lg:text-left text-lg">
 					Gestionale per palestre che rivoluziona l'allenamento dei tuoi clienti. Crea programmi personalizzati, monitora i progressi in tempo reale e migliora l'esperienza fitness direttamente dall’app.
 				</p>
 
-				<div class="flex items-center gap-2">
+				<div class="flex items-center gap-2 mt-3 justify-center lg:justify-start">
 					@auth
 						<a href="{{ route('training.select-day') }}" class="primary-button border-primary-800">
 							Entra nell'app
@@ -37,14 +37,14 @@
 							Login
 						</a>
 					@endauth
-				  	<a href="/" class="secondary-button border-secondary-800">
+				  	<a href="mailto:info@gymbro.com" class="secondary-button border-secondary-800">
 						Scopri di più
 					</a>
 				</div>
 			</div>
 						
-			<div class="w-1/2 shadow-lg border rounded-lg border-gray-800">
-				<img class="rounded-t-lg" src="{{ asset('images/home/topbar.png') }}" />
+			<div class="w-full p-6 lg:w-1/2">
+				<img class="border-gray-800 border-y-lg rounded-t-lg" src="{{ asset('images/home/topbar.png') }}" />
 
 				<!-- Pesca una foto random -->
 				@php
@@ -57,8 +57,15 @@
 					$random = rand(0, count($images) - 1);
 				@endphp
 			
-				<img class="rounded-b-lg" src="{{ $images[$random] }}" />
+				<img class="shadow-lg border-gray-800 border-y-lg rounded-b-lg" src="{{ $images[$random] }}" />
 			</div>
 		</div>
+
+		<footer class="bottom fixed bottom-0 w-full bg-black bg-opacity-50 text-gray-400 text-center py-3">
+			<x-mdi-xml class="inline-block h-6 w-6" />
+			with
+			<x-application-logo class="inline-block fill-gray-400 h-7 w-7" />
+			 by Biagio, Leonardo, Sebastian
+		</footer>
 	</body>
 </html>
